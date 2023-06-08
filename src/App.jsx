076aaee1b99808224b426fa54844d2b43d2f89d2
tarek-kidwai <tarek.kid@gmail.com>
2023-06-09 02:33:41 +1000

    fetch method bootstrap added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// functional components
+// import export
+// the theory is 1 file =  1 function / 1 UI
+// 1 file for delete button
+// 1 file for edit btn
+// 1 file for adding task btn
+// 1 file for rendering the task list
+// how do you join and compile these files together ?
+// we  use the import export feature es6 js
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  var name = "Someone";
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+  useEffect(() => {
+    axios
+      .get("https://rickandmortyapi.com/api/character")
+      .then((res) => console.log(res));
+  }, []);
 
-export default App
+  // useEffect(
+  //   ()=>{},
+  //   []
+  // )
+
+  return <div className="">Hello {name}</div>;
+};
+
+export default App;
